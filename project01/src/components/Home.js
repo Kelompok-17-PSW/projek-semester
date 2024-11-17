95e7c4bef7a50dae57c3c774e0c804f8e7a44e76
KasihImmanuela <kasihimmanuelabrsamosir@gmail.com>
2024-11-17 16:07:00 +0700

    Create Home.js

---
+++ project01/src/components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './pages.css';
+
+const Home = () => {
+  return (
+    <div className="home-background">
+      <div className="overlay"></div> 
+      <Container className="content">
+        <h1 className="display-1 fw-bold">Welcome to E-Learn</h1>
+        <p className="display-5">The best English learning website for you</p>
+        <p className="lead fw-bold">Khusus kelas 12 SMA</p>
+        <Button as={Link} to="/register" style={{ backgroundColor: 'black', borderColor: 'black' }} size="lg">
+          Mulai Sekarang
+        </Button>
+      </Container>
+    </div>
+  );
+}
+
+export default Home;
