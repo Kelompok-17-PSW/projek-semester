ce764b086676a8f13cbaf9d6e94f42bd5cb470bc
KasihImmanuela <kasihimmanuelabrsamosir@gmail.com>
2024-11-17 16:07:09 +0700

    Update App.js

--- project01/src/App.js
+++ project01/src/App.js
@@ -1,10 +1,28 @@
-import './App.css';
-import
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Dashboard from './components/Dashboard';
+import Login from '../components/Login';
+import Register from './components/Register';
+import Materi from './components/Materi';
+import Informasi from './components/Informasi';
+import Navbar from './components/Navbar';
+
+const App = () => {
   return (
-    <div >
-      
-    </div>
+    <Router>
+      <div>
+        <Navbar /> 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/materi" element={<Materi />} />
+          <Route path="/informasi" element={<Informasi />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
