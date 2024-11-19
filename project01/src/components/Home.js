dda95872417b6305b8626e5011a6c24733d8c481
KasihImmanuela <kasihimmanuelabrsamosir@gmail.com>
2024-11-19 20:58:15 +0700

    Update Home.js

--- project01/src/components/Home.js
+++ project01/src/components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import './pages.css';
+import '../css/Home.css';
 
 const Home = () => {
   return (
