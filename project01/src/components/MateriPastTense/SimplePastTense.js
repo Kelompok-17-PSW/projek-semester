445eeb9ce03fd3a2da55c51ff12164408cd8c86d
Paulus0612 <paulusmanurung385@gmail.com>
2024-11-28 14:30:40 +0700

    Create SimplePastTense.js

---
+++ project01/src/components/MateriPastTense/SimplePastTense.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const SimplePastTense = () => {
+  return (
+    <Container className="mt-4">
+      <h1><strong>Simple Past Tense</strong></h1>
+      Simple Past Tense adalah tenses yang digunakan untuk mengungkapkan suatu tindakan atau kejadian yang terjadi di masa lalu. Jadi, kejadian yang diceritakan, dimulai dan berakhir di masa lalu.
+      Tasya: Hello, Bagus! 
+      Bagus: Hi, Tasya!
+      Tasya: You sang beautifully on the competition yesterday. The teacher must be so proud of you!
+      Bagus: Oh, thank you, Tasya! That’s so kind of you.
+      <h4>Apa itu simple past tense?</h4>
+      <p>Simple Past Tense adalah tenses yang digunakan untuk mengungkapkan suatu tindakan atau kejadian yang terjadi di masa lalu. Jadi, kejadian yang diceritakan, dimulai dan berakhir di masa lalu.</p>
+      <h4><strong>Kegunaan Simple Past Tense</strong></h4>
+      <p></p>
+    </Container>
+  );
+}
+
+export default SimplePastTense;
