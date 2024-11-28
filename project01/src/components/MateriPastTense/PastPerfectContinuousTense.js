641f487f6bc15eeded08fcab270a06a6117077b3
Paulus0612 <paulusmanurung385@gmail.com>
2024-11-28 14:30:26 +0700

    Create PastPerfectContinuousTense.js

---
+++ project01/src/components/MateriPastTense/PastPerfectContinuousTense.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const PastPerfectContinuousTense = () => {
+  return (
+    <Container className="mt-4">
+      <h2>Past Perfect Continuous Tense</h2>
+      <p>Penjelasan mengenai Past Perfect Continuous Tense...</p>
+    </Container>
+  );
+}
+
+export default PastPerfectContinuousTense;
