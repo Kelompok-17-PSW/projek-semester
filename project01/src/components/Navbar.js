7432bc95b7fa3a48257e0d393aa1ba19c51a418e
Paulus0612 <paulusmanurung385@gmail.com>
2024-12-03 10:23:37 +0700

    Update Navbar.js

--- project01/src/components/Navbar.js
+++ project01/src/components/Navbar.js
@@ -10,15 +10,11 @@ const Navbar = () => {
         <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
         <BootstrapNavbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/" className="fs-5">Home</Nav.Link>
             <Nav.Link as={Link} to="/dashboard" className="fs-5">Dashboard</Nav.Link>
             <Nav.Link as={Link} to="/materi" className="fs-5">Materi</Nav.Link>
             <Nav.Link as={Link} to="/informasi" className="fs-5">Informasi</Nav.Link>
           </Nav>
-          <Nav>
-            <Nav.Link as={Link} to="/login" className="fs-5">Masuk</Nav.Link>
-            <Nav.Link as={Link} to="/register" className="fs-5">Daftar</Nav.Link>
-          </Nav>
+          
         </BootstrapNavbar.Collapse>
       </Container>
     </BootstrapNavbar>
